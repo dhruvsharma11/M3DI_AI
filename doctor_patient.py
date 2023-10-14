291abfb4c9f32d6d1f8a720e9684897863d2666c
kirtibansal2002 <kirtibansalva@gmail.com>
2023-10-14 16:28:41 -0400

    converted audio to text

--- doctor_patient.py
+++ doctor_patient.py
@@ -1,2 +1,12 @@
-import streamlit as st
-st.title("Doctor Patient App")
+# import streamlit as st
+# st.title("Doctor Patient App")
+
+import whisper
+
+def convert_audio_to_text(audio_file):
+    model = whisper.load_model("base")
+    result = model.transcribe(audio_file)
+    print(result["text"])
+
+convert_audio_to_text("Test.wav")
+
